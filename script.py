78f86bc8e76b552469d693c779007a59e7286d3e
Sonam Thakkar <sonamthakkar@sonams-mbp.lan>
2023-01-28 14:26:45 -0800

    completed lab

---
+++ script.py
@@ -0,0 +1,150 @@
+# exercise-01 Vowel or Consonant
+
+# Write the code that:
+# 1. Prompts the user to enter a letter in the alphabet:
+#      Please enter a letter from the alphabet (a-z or A-Z):
+# 2. Write the code that determines whether the letter entered is a vowel
+# 3. Print one of following messages (substituting the letter for x):
+#      - The letter x is a vowel
+#      - The letter x is a consonant
+# Hints:  Use the in operator to check if a character is in another string
+#         For example, if some_char in 'abc':
+# vowel = input(f'Enter alphabet ').lower()
+# print(f'the user entered {vowel}')
+
+if vowel in 'aeiou':
+  print(f'letter {vowel} is a vowel')
+else:
+  print(f'Letter {vowel} is a consonant')
+# exercise-02 Length of Phrase
+
+# Write the code that:
+# 1. Prompts the user to enter a phrase:
+#      Please enter a word or phrase:
+# 2. Print the following message:
+#      - What you entered is xx characters long
+# 3. Return to step 1, unless the word 'quit' was entered.
+# phrase = input(f'Enter a phrase: ').lower()
+# print(f'the user entered {phrase}')
+# print(len(phrase))
+
+
+# exercise-03 Calculate Dog Years
+
+# Write the code that:
+# 1. Prompts the user to enter a dog's age in human years like this:
+#      Input a dog's age in human years:
+# 2. Calculates the equivalent dog years, where:
+#      - The first two years count as 10 years each
+#      - Any remaining years count as 7 years each
+# 3. Prints the answer in the following format:
+#      The dog's age in dog years is xx
+# Hint:  Use the int() function to convert the string returned from input() into an integer
+# d_age = int(input(f'Enter dogs age: '))
+# print(f'the user entered {d_age}')
+
+if d_age < 0:
+	print("Age must be positive number.")
+	exit()
+elif d_age <= 2:
+	h_age = d_age * 10
+else:
+	h_age = d_age * 7
+
+print("The dog's age in human's years is", h_age)
+
+
+
+# exercise-04 What kind of Triangle?
+
+# Write the code that:
+# 1. Prompts the user to enter the three lengths of a triangle (one at a time):
+#      Enter the lengths of three side of a triangle:
+#      a:
+#      b:
+#      c:
+# 2. Write the code that determines if the triangle is:
+#      equalateral - all three sides are equal in length
+#      scalene - all three sides are unequal in length
+#      isosceles - two sides are the same length
+# 3. Print a message such as:
+#      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+
+print("Input lengths of the triangle sides: ")
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+
+if x == y == z:
+	print("Equilateral triangle")
+elif x==y or y==z or z==x:
+	print("isosceles triangle")
+else:
+	print("Scalene triangle")
+
+# exercise-05 Fibonacci sequence for first 50 terms
+
+# Write the code that:
+# 1. Calculates and prints the first 50 terms of the fibonacci sequence.
+# 2. Print each term and number as follows:
+#      term: 0 / number: 0
+#      term: 1 / number: 1
+#      term: 2 / number: 1
+#      term: 3 / number: 2
+#      term: 4 / number: 3
+#      term: 5 / number: 5
+#      etc.
+# Hint: The next number is found by adding the two numbers before it
+
+x=0
+y=1
+print(x+y)
+while y<50:
+    print(y)
+    x=y
+    y=x+y
+    
+
+# exercise-06 What's the  Season?
+
+# Write the code that:
+# 1. Prompts the user to enter the month (as three characters):
+#      Enter the month of the season (Jan - Dec):
+# 2. Then promptshtt the user to enter the day of the month:
+#      Enter the day of the month:
+# 3. Calculate what season it is based upon this chart:
+#      Dec 21 - Mar 19: Winter
+#      Mar 20 - Jun 20: Spring
+#      Jun 21 - Sep 21: Summer
+#      Sep 22 - Dec 20: Fall
+# 4. Print the result as follows:
+#      <Mmm> <dd> is in <season>
+
+# Hints:
+# Consider using the in operator to check if a string is in a particular list/tuple like this:
+# if input_month in ('Jan', 'Feb', 'Mar'):
+# After setting the likely season, you can use another if...elif...else statement to "adjust" if
+# the day number falls within a certain range.
+
+month = input("Input the month: ")
+day = int(input("Input the day: "))
+
+if month in ('January', 'February', 'March'):
+	season = 'winter'
+elif month in ('April', 'May', 'June'):
+	season = 'spring'
+elif month in ('July', 'August', 'September'):
+	season = 'summer'
+else:
+	season = 'autumn'
+
+if (month == 'March') and (day > 19):
+	season = 'spring'
+elif (month == 'June') and (day > 20):
+	season = 'summer'
+elif (month == 'September') and (day > 21):
+	season = 'autumn'
+elif (month == 'December') and (day > 20):
+	season = 'winter'
+
+print("Season is",season)
